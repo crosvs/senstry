db13fe79c2ecd6c924941454536367c19e70c006
Crosvs <github.wielder464@passmail.com>
2026-04-22 03:19:07 +0200

    404 fix

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
-		adapter: adapter({ fallback: 'index.html' }),
+		adapter: adapter({ fallback: '404.html' }),
 		paths: { base }
 	}
 };
